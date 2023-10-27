e5670cc9d7a40c20233060b61d63cae257a90412
GabyAM <gdgabyam@gmail.com>
2023-10-27 10:16:13 -0300

    added changing size functionality
    the user now can change the size of the grid throught an input. This does not change the size of the grid but of the pixels

--- index.js
+++ index.js
@@ -5,24 +5,40 @@ function resetGrid() {
     })
 }
 
-function createGrid() {
+function getGridSize() {
+
+}
+function createGrid(gridSize = 256) {
     const $grid = document.querySelector('.grid');
-    for(let i=0; i<256; i++) {
+    for(let i=0; i<gridSize; i++) {
         const newDiv = document.createElement('div')
         newDiv.classList.add('pixel');
         newDiv.addEventListener("mouseover", () => {
             newDiv.style.backgroundColor = 'black'
         })
-        newDiv.style.width = '20px';
-        newDiv.style.height = '20px';
+
+        const size = 320 / (Math.sqrt(gridSize))
+        newDiv.style.width = `${size}px`;
+        newDiv.style.height = `${size}px`;
         $grid.appendChild(newDiv);
     }
+}
+
+function changeGridSize() {
+    const gridSize = document.querySelector('#grid-size-input').value;
+
+    const $grid = document.querySelector('.grid');
+    $grid.innerHTML = '';
+    createGrid(Math.pow(gridSize, 2));
 }
 
 function init() {
     createGrid()
     const $resetButton = document.querySelector('.reset-button');
     $resetButton.addEventListener('click', resetGrid);
+
+    const $changeSizeButton = document.querySelector('.change-size-button');
+    $changeSizeButton.addEventListener('click', changeGridSize)
 }
 
 init()
