d4e266f393cd151e7ec31d893c7ce47520a49f7b
GabyAM <gdgabyam@gmail.com>
2023-10-27 09:28:56 -0300

    adds interactivity and reset button
    when hovering the pixels they are now painted black, and when pressing the reset button all the pixels go back to white.

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
+function resetGrid() {
+    const pixels = document.querySelectorAll('.pixel');
+    pixels.forEach(pixel => {
+        pixel.style.backgroundColor = 'white';
+    })
+}
+
 function createGrid() {
     const $grid = document.querySelector('.grid');
     for(let i=0; i<256; i++) {
         const newDiv = document.createElement('div')
-        newDiv.addEventListener('hover', () => {
+        newDiv.classList.add('pixel');
+        newDiv.addEventListener("mouseover", () => {
             newDiv.style.backgroundColor = 'black'
         })
-        newDiv.style.width = '10px';
-        newDiv.style.height = '10px';
+        newDiv.style.width = '20px';
+        newDiv.style.height = '20px';
         $grid.appendChild(newDiv);
     }
 }
 
-createGrid()
+function init() {
+    createGrid()
+    const $resetButton = document.querySelector('.reset-button');
+    $resetButton.addEventListener('click', resetGrid);
+}
+
+init()
