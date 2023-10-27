98819b5547c8a25643111261c9ad34f5f970c2a2
GabyAM <gdgabyam@gmail.com>
2023-10-27 10:33:05 -0300

    adds color mode
    theres now a button that changes the painting mode from black to a random color

--- index.js
+++ index.js
@@ -5,17 +5,31 @@ function resetGrid() {
     })
 }
 
-function getGridSize() {
-
+function getRandomRGB() {
+    const red = Math.floor(Math.random() * 255);
+    const green = Math.floor(Math.random() * 255);
+    const blue = Math.floor(Math.random() * 255);
+    return `rgb(${red}, ${green}, ${blue})`
 }
+
+function setPixelColor(pixel, color) {
+    pixel.style.backgroundColor = color;
+}
+
+function updateColor() {
+    const $pixels = document.querySelectorAll('.pixel');
+    $pixels.forEach(pixel => {
+        pixel.removeEventListener('mouseover', () => setPixelColor(newDiv, 'black'))
+        pixel.addEventListener('mouseover', () => setPixelColor(pixel, getRandomRGB()))
+    })
+}
+
 function createGrid(gridSize = 256) {
     const $grid = document.querySelector('.grid');
     for(let i=0; i<gridSize; i++) {
         const newDiv = document.createElement('div')
         newDiv.classList.add('pixel');
-        newDiv.addEventListener("mouseover", () => {
-            newDiv.style.backgroundColor = 'black'
-        })
+        newDiv.addEventListener("mouseover", () => setPixelColor(newDiv, 'black'))
 
         const size = 320 / (Math.sqrt(gridSize))
         newDiv.style.width = `${size}px`;
@@ -39,6 +53,9 @@ function init() {
 
     const $changeSizeButton = document.querySelector('.change-size-button');
     $changeSizeButton.addEventListener('click', changeGridSize)
+
+    const $colorModeButton = document.querySelector('.color-mode-button');
+    $colorModeButton.addEventListener('click', updateColor)
 }
 
 init()
